874298aa365dbc601345b9b4069bce033c69e451
Csaba Vadász <77699103+Csaba79-coder@users.noreply.github.com>
2021-10-10 18:35:51 +0200

    Initial commit

---
+++ spring5-mvn-jokes-app/src/main/java/guru/springframework/spring5mvnjokesapp/Spring5MvnJokesAppApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5mvnjokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5MvnJokesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5MvnJokesAppApplication.class, args);
+	}
+
+}
---
+++ spring5-mvn-jokes-app/src/test/java/guru/springframework/spring5mvnjokesapp/Spring5MvnJokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5mvnjokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5MvnJokesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
