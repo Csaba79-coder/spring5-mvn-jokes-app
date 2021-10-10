a7bd5960a057f5de62d52fe3e771d9908cfefed2
Csaba Vadász <77699103+Csaba79-coder@users.noreply.github.com>
2021-10-10 20:03:22 +0200

    creating joke controller. closes #5

---
+++ spring5-mvn-jokes-app/src/main/java/guru/springframework/spring5mvnjokesapp/controllers/JokeController.java
@@ -0,0 +1,27 @@
+package guru.springframework.spring5mvnjokesapp.controllers;
+
+import guru.springframework.spring5mvnjokesapp.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    // @Autowired is automatically injects it
+    // we can admit Autowired annotation as we have a single constructor in the class!
+    // Spring will automatically do the dependency injection!!! in this case!
+    // @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+        model.addAttribute("joke", jokeService.getJoke());
+        return "index";
+    }
+}
